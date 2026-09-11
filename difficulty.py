a248fb31fc211cf3f510787b661d8acb9adb13bb
AAMunta <anbdullah856@gmail.com>
2026-09-11 14:44:42 +0600

    level and Word manager

---
+++ difficulty.py
@@ -0,0 +1,43 @@
+Level={
+    1: {
+        "name": "Easy",
+        "min_frequency": 6.00,
+        "max_frequency": 7.73
+    },
+
+    2: {
+        "name": "Normal",
+        "min_frequency": 4.50,
+        "max_frequency": 5.99
+    },
+
+    3: {
+        "name": "Intermediate",
+        "min_frequency": 3.50,
+        "max_frequency": 4.49
+    },
+
+    4: {
+        "name": "Advanced",
+        "min_frequency": 2.50,
+        "max_frequency": 3.49
+    },
+
+    5: {
+        "name": "Expert",
+        "min_frequency": 2.00,
+        "max_frequency": 2.49
+    },
+
+    6: {
+        "name": "Master",
+        "min_frequency": 1.50,
+        "max_frequency": 1.99
+    },
+
+    7: {
+        "name": "Legend",
+        "min_frequency": 1.01,
+        "max_frequency": 1.49
+    }
+}
---
+++ word_manager.py
@@ -0,0 +1,41 @@
+import csv
+import random
+from difficulty import Level
+class WordManager:
+    def __init__(self,filename="Guess-the-word/words.csv"):
+        self.filename=filename
+        self.words=self.load_words()
+        self.level_pools={}
+        self.prepare_pools()
+    def load_words(self):
+        with open(self.filename,"r",encoding="utf-8")as file:
+            reader=csv.DictReader(file)
+            return list(reader)
+    def prepare_pools(self):
+        for level in Level:
+            self.level_pools[level]=[]
+
+        for row in self.words:
+            frequency=float(row["frequency"])
+
+            for level, data in Level.items():
+                if(data["min_frequency"]<=frequency<=data["max_frequency"]):
+                    self.level_pools[level].append(row)
+                    break
+        for level in self.level_pools:
+            random.shuffle(self.level_pools[level])
+
+    def get_random_word(self,level):
+        pool=self.level_pools[level]
+
+        if not pool:
+            self.refill_pool(level)
+        return pool.pop()
+    def refill_pool(self,level):
+        for row in self.words:
+            frequency=float(row["frequency"])
+            data=Level[level]
+            if(data["min_frequency"]<=frequency<=data["max_frequency"]):
+                self.level_pools[level].append(row)
+
+        random.shuffle(self.level_pools[level])
