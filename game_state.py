81e32b4371a3b7a1bb0f0d4979f6d58d96736e92
SaquibSinan <snazmussaquibsinan@gmail.com>
2026-09-23 03:54:38 +0600

    Refactor GameState initialization and add hints logic
    Refactor GameState class initialization and add hint management methods.

--- game_state.py
+++ game_state.py
@@ -1,22 +1,49 @@
 class GameState:
-    def __init__(self,word_data,level):
-        self.word=word_data["word"].lower()
-        self.length=int(word_data["Length"])
-        self.level=level
+    def __init__(self, word_data, level):
+        self.word = word_data["word"].lower()
+        self.length = int(word_data["Length"])
+        self.level = level
 
-        self.revealed=[False]*self.length
+        self.revealed = [False] * self.length
 
-        self.incorrect_attempts=0
-        self.total_attempts=0
+        self.incorrect_attempts = 0
+        self.total_attempts = 0
 
-        self.won=False
-        self.forfeited=False
+        self.hints_used = 0
+        self.max_hints = self.get_max_hints()
+
+        self.won = False
+        self.forfeited = False
+
+    def get_max_hints(self):
+        if self.level in [1, 2]:
+            return 2
+        elif self.level in [3, 4]:
+            return 3
+        elif self.level in [5, 6]:
+            return 4
+        elif self.level == 7:
+            return 5
+
+        return 0
 
     def revealed_count(self):
         return sum(self.revealed)
-    def maximum_revealed_count(self):
-        return round(0.4*self.length)
-    def remaining_reveal_capacity(self):
-        return int(self.maximum_revealed_count()-self.revealed_count())
+
+    def clue_available(self):
+        return (
+            self.incorrect_attempts >= 5
+            and self.hints_used < self.max_hints
+        )
+
+    def use_hint(self):
+        if not self.clue_available():
+            return False
+
+        self.hints_used += 1
+        self.incorrect_attempts = 0
+
+        return True
+
     def reveal_all(self):
-        self.revealed=[True]*self.length
+        self.revealed = [True] * self.length
